cac4547d3d680b09f0f21aea62dca88443b246fc
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-06-14 22:18:39 +0000

    finished

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,46 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg bg-warning">
+		<div className="container-fluid ps-4">
+			<a className="navbar-brand" href="#">LOGO</a> {/*link to home page*/}
+			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+				<li className="nav-item dropdown">
+				<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+					User {/*user name profile */}
+				</a>
+				<ul className="dropdown-menu">
+					<li><a className="dropdown-item" href="#">Profile</a></li>
+					<li><a className="dropdown-item" href="#">Home</a></li>
+					<li><a className="dropdown-item" href="#">Games</a></li>
+					<li><a className="dropdown-item" href="#">Reviews</a></li>
+					<li><a className="dropdown-item" href="#">Likes</a></li>
+					<li><hr className="dropdown-divider"/></li>
+					<li><a className="dropdown-item" href="#">Settings</a></li>
+					<li><a className="dropdown-item text-danger" href="#">Sing Out</a></li>
+					
+				</ul>
+				</li>
+				<li className="nav-item">
+				<a className="nav-link active" aria-current="page" href="#">Game</a>
+				</li>
+				<li className="nav-item">
+				<a className="nav-link" href="#">List</a>
+				</li>
+				<li className="nav-item">
+				<a className="nav-link disabled" aria-disabled="true">Disabled</a> {/*maybe members */}
+				</li>
+			</ul>
+			<form className="d-flex" role="search">
+				<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> {/*add search icon*/}
+				<button className="btn btn-outline-secondary pe-4" type="submit">Search</button>
+			</form>
 			</div>
+		</div>
 		</nav>
 	);
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,172 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      {/*welcome back user */}
+      <div className="card text-bg-dark">
+        <img src={rigoImageUrl} className="card-img" alt="..." />
+        <div className="card-img-overlay align-bottom">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <button type="button" class="btn btn-light">
+            Sing up!
+          </button>
+        </div>
+      </div>
+      <br></br>
+      <div className="row gameList row- gx-4">
+        {/*games your friends have recently reviewed/played*/}
+        <div className="col-auto d-flex mx-auto p-4 gap-4 ">
+          <div className="card mx-auto p-2 " style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2 " style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2" style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2" style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+        </div>
+      </div>
+	  <br></br>
+      <div className="row gameList row- gx-4">
+        {/*new releases*/}
+        <div className="col-auto d-flex mx-auto p-4 gap-4 ">
+          <div className="card mx-auto p-2 " style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2 " style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2" style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+          <div className="card mx-auto p-2" style={{ marginRight: "18rem" }}>
+            <img src={rigoImageUrl} className="card-img-top" alt="..." />
+          </div>
+        </div>
+      </div>
+	  <div className="row g-0">
+        <div className="card my-3 mx-auto" style={{ maxWidth: "540px" }}>
+          <div className="d-flex">
+            <div className="col-md-4">
+              <img
+                src={rigoImageUrl}
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Reviews</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  <div className="row g-0">
+        <div className="card my-3 mx-auto" style={{ maxWidth: "540px" }}>
+          <div className="d-flex">
+            <div className="col-md-4">
+              <img
+                src={rigoImageUrl}
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Reviews</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  <div className="row g-0">
+        <div className="card my-3 mx-auto" style={{ maxWidth: "540px" }}>
+          <div className="d-flex">
+            <div className="col-md-4">
+              <img
+                src={rigoImageUrl}
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Reviews</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  <div className="row g-0">
+        <div className="card my-3 mx-auto" style={{ maxWidth: "540px" }}>
+          <div className="d-flex">
+            <div className="col-md-4">
+              <img
+                src={rigoImageUrl}
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Reviews</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
