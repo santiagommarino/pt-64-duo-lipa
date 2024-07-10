555c98d631efb3dcfb58fc866b36afa686d4f365
NathanMB77 <160797692+NathanMB77@users.noreply.github.com>
2024-07-10 11:57:34 -0400

    Revert "search page works. next step is finalize reviews"

--- src/front/js/pages/homeLoggedOff.js
+++ src/front/js/pages/homeLoggedOff.js
@@ -7,8 +7,9 @@ import { SignupModal } from "../component/signupModal.js";
 import { LoginModal } from "../component/loginModal.js";
 
 
-export const HomeLoggedOff = ({}) => {
+export const HomeLoggedOff = ({ logout }) => {
   const { store, actions } = useContext(Context);
+  const [userInfo, setUserInfo] = useState(null);
   const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const popularGameElements = [];
