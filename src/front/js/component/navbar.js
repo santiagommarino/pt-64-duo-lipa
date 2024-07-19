4caf6f214c775e7f5c831ca864661fe3051978cd
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-07-19 09:37:18 -0400

    update navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,58 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { Context } from "../store/appContext";
-import { useContext } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SignupModal } from "./signupModal";
 import { LoginModal } from "./loginModal";
 
-
 export const Navbar = () => {
 
-  const { store, actions } = useContext(Context);
-  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-  const [userInfo, setUserInfo] = useState({});
 
-  useEffect(() => {
-    actions.handleFetchUserInfo();
-  }, []);
-
-  useEffect(() => {
-    setUserInfo(sessionStorage.getItem("userInfo"));
-  }, [store.user]);
-
-  const handleSignupModal = () => {
-    isSignupModalOpen ? setIsSignupModalOpen(false) : setIsSignupModalOpen(true);
-  };
-
-  const handleLoginModal = () => {
-    isLoginModalOpen ? setIsLoginModalOpen(false) : setIsLoginModalOpen(true);
-  };
-
-  const handleLogOut = () => {
-    actions.handleLogOut();
-  }
+  const [showSingUp, setShowSingUp] = useState(false)
+  const [showLogIn, setShowLogIn] = useState(false)
+  const handleCloseSingUp = () => setShowSingUp(false)
+  const handleCloseLogIn = () => setShowLogIn(false)
+  const handleShowSingUp = () => setShowSingUp(true)
+  const handleShowLogIn = () => setShowLogIn(true)
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg ps-4">
+      <nav className="navbar navbar-expand-lg bg-warning ps-4">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">LOGO</a>
-          {userInfo ?
-            <button onClick={handleLogOut} type="button" className="btn btn-primary ms-2">Log out</button>
-            :
-            <>
-              <button className="btn btn-primary ms-2" onClick={handleSignupModal}>Sign up</button>
-              <button className="btn btn-primary ms-2" onClick={handleLoginModal}>Log in</button>
-            </>
-          }
-          <Link to="/search" className="btn btn-primary">Search</Link>
-          <Link to="/" className="btn btn-primary">Home</Link>
-          <Link to="/private" className="btn btn-primary">Private</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <button className="btn btn-primary ms-2" onClick={handleShowLogIn}>Sign in</button>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-primary ms-2" onClick={handleShowSingUp}>Sign Up</button>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <div className="d-flex align-items-center">
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-primary" type="submit"><i class="bi bi-search"></i></button>
+              </form>
+            </div>
+          </div>
         </div>
       </nav>
-      {isLoginModalOpen && <LoginModal closeModal={handleLoginModal} />}
-      {isSignupModalOpen && <SignupModal closeModal={handleSignupModal} />}
+      <LoginModal closeModal={handleCloseLogIn} show={showLogIn} />
+      <SignupModal closeModal={handleCloseSingUp} show={showSingUp} />
     </>
   );
 };
